05b732d6bc70172a76a3e62846ca80ee98431a27
Mustafa Erbay <72121845+mustafaerbay@users.noreply.github.com>
2020-12-03 20:49:39 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+proxy=$1
+
+docker run --user root --env http_proxy=http://$proxy --env https_proxy=http://$proxy -d --restart on-failure -v /data01/jenkins_home:/var/jenkins_home -v /data01/jenkins_sshkeys:/root/.ssh -p 8080:8080 -p 50000:50000 jenkinsci/blueocean:latest
