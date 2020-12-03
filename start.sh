5668627fbcc2c4281e2f823f25c4d23e257965de
Mustafa Erbay <72121845+mustafaerbay@users.noreply.github.com>
2020-12-03 20:55:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 proxy=$1
 
-if [ X == "$proxy"X]; then
+if [ X == "$proxy"X ]; then
   docker run --user root --env http_proxy=http://$proxy --env https_proxy=http://$proxy -d --restart on-failure -v /data01/jenkins_home:/var/jenkins_home -v /data01/jenkins_sshkeys:/root/.ssh -p 8080:8080 -p 50000:50000 jenkinsci/blueocean:latest
 else
   docker run --user root -d --restart on-failure -v /data01/jenkins_home:/var/jenkins_home -v /data01/jenkins_sshkeys:/root/.ssh -p 8080:8080 -p 50000:50000 jenkinsci/blueocean:latest
